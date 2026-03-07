fbd341c907ee5af4495c236ef6e4ca3a0ec952bd
yarik127123 <nikdu54@gmail.com>
2026-03-07 13:31:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,114 @@
-from customtkinter import*
+from customtkinter import *
 
 
-
-Class MainWindow(CTk):
+class MainWindow(CTk):
     def __init__(self):
-        super().__init--()
-        self.geometry("700x500")
-        self.title("logtalk")
+        super().__init__()
 
 
+        self.geometry('400x600')
+        self.title("Chat Client")
 
-        self.menu_btn = CTkButton(self,width=200,text="menu",corner_radius=0)
-        self.menu_btn.place(x=0,y=0)
 
-        self.menu_frame = CTkframe(self,width=200,height=500,corner_radius=0)
+        # Меню
+        self.label = None
+        self.menu_frame = CTkFrame(self, width=30, height=300)
+        self.menu_frame.pack_propagate(False)
         self.menu_frame.place(x=0, y=0)
+        self.is_show_menu = False
+        self.speed_animate_menu = -20
+        self.btn = CTkButton(self, text='▶️', width=30)
+        self.btn.place(x=0, y=0)
 
-        self.chat_frame = CTkScrollableFrame(self)
-        self.chat_frame.place(x=0. y=9)
 
-        self.send_frame = CTkFrame(Self)
-        self.senf_frame.place(x=0,y=0)
 
-        self.pin_btn = CTkButton(self,self.send_frame,text="pin")
-        self.pin_btn = C
+
+        # Основне поле чату
+        self.chat_field = CTkScrollableFrame(self)
+        self.chat_field.place(x=0, y=0)
+
+
+
+
+        # Поле введення та кнопки
+        self.message_entry = CTkEntry(self, placeholder_text='Введіть повідомлення:', height=40)
+        self.message_entry.place(x=0, y=0)
+        self.send_button = CTkButton(self, text='>', width=50, height=40)
+        self.send_button.place(x=0, y=0)
+
+
+
+
+        self.open_img_button = CTkButton(self, text='📂', width=50, height=40)
+        self.open_img_button.place(x=0, y=0)
+
+
 
 
         self.adaptive_ui()
+       
+
 
     def adaptive_ui(self):
-        self.menu_frame.place(x = 0, y = self.menu_btn.winfo_height())
-        self.after(50,self.adaptive_ui)
+        self.menu_frame.configure(height=self.winfo_height())
+        self.chat_field.place(x=self.menu_frame.winfo_width())
+        self.chat_field.configure(width=self.winfo_width() - self.menu_frame.winfo_width() - 20,
+                                    height=self.winfo_height() - 40)
+        self.send_button.place(x=self.winfo_width() - 50, y=self.winfo_height() - 40)
+        self.message_entry.place(x=self.menu_frame.winfo_width(), y=self.send_button.winfo_y())
+        self.message_entry.configure(
+            width=self.winfo_width() - self.menu_frame.winfo_width() - 110)
+        self.open_img_button.place(x=self.winfo_width()-105, y=self.send_button.winfo_y())
 
 
 
-main = MainWindow()
-main.mainloop()
+
+        self.after(50, self.adaptive_ui)
+
+    def toggle_menu(self):
+        if self.is_show_menu:
+            self.is_show = False
+            self.speed_animate_menu *= -1
+            self.btn.configure(text = "")
+            self.show_menu()
+        else:
+            self.is_show_menu = True
+            self.speed_animate_menu *= -1
+            self.btn.configure(text = "MENU")
+            self.show_menu()
+
+            self.label = CTklabel(self.menu_frame, text="ім'я")
+            self.label.pack(pady=30)
+            
+            self.entry = CTkEntry(self.mene_frame,placeholder_text="ваш нік")
+            self.entry.pack()
+            self.save_btn = CTkButton(self.menu_frame, text="Зберегти", command=self.save_name)
+            self.save_btn.pack()
+        
+    def show_menu(self):
+        self.menu_frame.configure(width = self.menu_frame.winfo_width() + self.speed_animate_menu)
+        if not self.menu_frame.winfo_width() >= 200 and self.is_show_menu:
+            self.after(10, self.show_menu)
+        elif self.menu_frame.winfo_width() >=60 and not self.is_show_menu:
+            self.after(10, self.show_menu)
+        if self.label: self.label.destroy()
+        if getattr(self, "entry",None): self.entry.destroy()
+        if getattr(self, "save_btn",None): self.save_btn.destroy()
+
+
+    def add_message(self, message, img=None):
+        message_frame = CTkFrame(self.chat_field, fg_color="gray")
+        message_frame.pack(pady=5, anchor="w" )
+
+        wrapleng_size = self.winfo_width()- self.menu_frame.winfo_width() - 40
+            CTkLabel(message_frame, text=message, wraplength=wrapleng_size,
+                    text_color='white',justify='left').pack(padx=10,pady=5)
+    else:
+        CTkLabel(message_frame, text=messege, wraplength=wrapleng_size,
+                text_color='white',image=img, compound='top',justify='left').pack(padx=10,pady=5)
+    
+
+
+n = MainWindow()
+n,mainloop()
+
